77058aef72bea7968a7d538ffdb807b5d93fa46c
dbreakers <david.breakwell@gmail.com>
2020-03-02 08:23:02 +0000

    Add files via upload

---
+++ osmrelay2.php
@@ -0,0 +1,30 @@
+<?php 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization");
+$osm = "https://www.onlinescoutmanager.com/";
+$ch = curl_init();
+$path = $_SERVER['REQUEST_URI'];
+$parameters = $_GET;
+$path_add = $parameters['osmpath'];
+unset($parameters['osmpath']);
+$path = http_build_query($parameters);
+
+// Insert Yours here 
+
+$_POST['apiid'] = 'XX';
+$_POST['token'] = 'YYYYYYYYYYYYYYYYYYYYYYY';
+
+
+curl_setopt($ch, CURLOPT_URL, 'https://www.onlinescoutmanager.co.uk/'.$path_add.'?'.$path);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($_POST));
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+echo curl_exec($ch);
+
+exit;
+
+
+
+?>
